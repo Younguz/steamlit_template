e2ae796839bbb9c6248e722384daf0bbd32f1483
Younguz <158337690+Younguz@users.noreply.github.com>
2024-04-25 18:06:46 -0400

    Update 1_🌍_Interactive_Map.py

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -3,7 +3,7 @@ import leafmap.foliumap as leafmap
 
 markdown = """
 A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
+<https://github.com/Younguz/steamlit_template>
 """
 
 st.sidebar.title("About")
