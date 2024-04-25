c1f92861b304fa964aa6d78c6a50e12751882d76
Younguz <158337690+Younguz@users.noreply.github.com>
2024-04-25 18:08:04 -0400

    Update 6_📦_Web_Map_Service.py

--- pages/6_📦_Web_Map_Service.py
+++ pages/6_📦_Web_Map_Service.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 
 markdown = """
 A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
+<https://github.com/Younguz/steamlit_template>
 """
 
 st.sidebar.title("About")
